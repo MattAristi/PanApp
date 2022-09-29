dee804eda0fdf08c1da583738c86cc353b590917
Matias Aristimuno <mattaristi@MacBook-Air-de-Ariel.local>
2022-09-29 14:45:41 +0100

    PanApp

---
+++ src/navigation/shop.js
@@ -0,0 +1,21 @@
+// import Categories from '../screens/categories'
+// import Product from '../screens/product';
+// import Products from '../screens/products';
+import {Categories, Product, Products} from '../screens/index'
+
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+const ShopNavigator = ()=> {
+    return (
+        <Stack.Navigator initialRouteName='Products'>
+            <Stack.Screen name='Categories' component={Categories}/>
+            <Stack.Screen name='Products' component={Products}/>
+            <Stack.Screen name='Product' component={Product}/>
+        </Stack.Navigator>
+    )
+}
+
+export default ShopNavigator
---
+++ src/screens/categories/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#fff',
+        justifyContent:"center",
+        alignItems: "center"
+    },
+    title:{
+        fontFamily:'Lato-Regular',
+    }
+})
