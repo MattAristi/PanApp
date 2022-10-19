ac48bfd32fe47174ee5cca148500c3caf3549fc8
Matias Aristimuno <mattaristi@MacBook-Air-de-Ariel.local>
2022-10-19 20:23:49 +0100

    index reduce

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import {categoryReducer, productReducer} from './reducers';
+import {categoryReducer, orderReducer, productReducer} from './reducers';
 import {combineReducers, createStore} from 'redux';
 
 const rootReducer = combineReducers({
     products : productReducer,
     category : categoryReducer,
+    order : orderReducer,
 })
 
 export default createStore(rootReducer);
